fcd7d1b3b289414d836aa4b3c72ca7f862a86f7d
Jairo Galvez <jairo.jggc@gmail.com>
2025-12-29 21:56:35 -0500

    fix: nuevas versiones rf providers

--- steps/02-supply/terraform/main/versions.tf
+++ steps/02-supply/terraform/main/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.41.0"
+      version = "4.57.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "3.5.0"
+      version = "3.7.0"
     }
     random = {
       source  = "hashicorp/random"
